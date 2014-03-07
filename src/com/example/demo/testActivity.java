d3e38908e2eb674afac249f328acd3a241193564
naoto <cht50200@gmail.com>
2014-03-07 17:56:22 +0900

    add seasonal breads

--- src/com/example/demo/testActivity.java
+++ src/com/example/demo/testActivity.java
@@ -10,7 +10,7 @@ public class testActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		//testActivity
+		//testActivityaaa
 	}
 
 	@Override
